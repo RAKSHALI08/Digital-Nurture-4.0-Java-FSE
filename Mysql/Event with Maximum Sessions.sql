acfb967c6cdc0eb8c55dd2794e36ac8139da6b66
RAKSHALI M A <119483785+RAKSHALI08@users.noreply.github.com>
2025-05-25 16:08:10 +0530

    Event with Maximum Sessions.sql

---
+++ Mysql/Event with Maximum Sessions.sql
@@ -0,0 +1,31 @@
+SELECT 
+    e.event_id,
+    e.title AS event_title,
+    COUNT(s.session_id) AS session_count
+FROM 
+    Events e
+JOIN 
+    Sessions s ON e.event_id = s.event_id
+GROUP BY 
+    e.event_id, e.title
+HAVING 
+    session_count = (
+        SELECT 
+            MAX(session_total)
+        FROM (
+            SELECT 
+                COUNT(session_id) AS session_total
+            FROM 
+                Sessions
+            GROUP BY 
+                event_id
+        ) AS session_counts
+    );
+
+
+
+output
+| event\_id | event\_title           | session\_count |
+| --------- | ---------------------- | -------------- |
+| 1         | Tech Innovators Meetup | 2              |
+
